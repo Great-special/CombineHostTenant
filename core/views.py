bb5f560e27ef3824300418b2fba2253a07f1e7ae
great-special <sirgreatspecial@gmail.com>
2024-10-14 18:51:48 +0100

    queried tenant data from public schema

--- core/views.py
+++ core/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-
+from django_tenants.utils import schema_context
+from django.apps import apps
+from .models import Client
 # Create your views here.
-
+UnitModel = apps.get_model('tenants', 'UnitModel')
 
 def index_view(request):
-    print()
-    return render(request, 'core.html')
+    tenants_set = set()
+    # Switch to the tenant's schema
+    tenants = Client.objects.all().exclude(schema_name='public')
+    print(tenants)
+    # Switch to the tenant's schema
+    for tenant in tenants:
+        with schema_context(tenant.schema_name):
+            # Perform queries on tenant-specific models
+            tenants_set.update(UnitModel.objects.all())
+    print(tenants_set)
+    return render(request, 'core.html', {'tenants_data':tenants_set})
+
+
+
+
+
 
--- tenants/models.py
+++ tenants/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class UnitModel(models.Model):
+    title = models.CharField(max_length=50)
+    info = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title
